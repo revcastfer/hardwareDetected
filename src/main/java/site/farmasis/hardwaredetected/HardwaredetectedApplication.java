f8a4816acc9d96a92324b9ab2f1acdffa84261cb
fernando <revcastfer@gmail.com>
2023-11-27 12:35:46 -0500

    correccion de lectura

--- src/main/java/site/farmasis/hardwaredetected/HardwaredetectedApplication.java
+++ src/main/java/site/farmasis/hardwaredetected/HardwaredetectedApplication.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,6 +22,8 @@ public class HardwaredetectedApplication {
      public static ArrayList<memoryRam> memorias=new ArrayList<>();
       public static ArrayList<disco> discos=new ArrayList<>();
       public static ArrayList<procesador> cpus=new ArrayList<>();
+      public static ArrayList<String> rpta = new ArrayList<>();
+      
       
   
 
@@ -87,9 +90,10 @@ public class HardwaredetectedApplication {
            
             while ((line2 = reader2.readLine()) != null) {
                 if (!line2.isEmpty()) {
-                    if (!headerSkipped2){headerSkipped2 = true;continue;}
+                    if (!headerSkipped2){headerSkipped2 = true;continue;}                   
                     String rpta2[]= line2.replaceAll("\\s+", " ").split(" ");
-                    disco nuevoDisco=new disco(rpta2[0], rpta2[1]+" "+rpta2[2]+" "+rpta2[3]+" "+rpta2[4], Double.valueOf(rpta2[5]));
+                    Integer lastPosition=rpta2.length - 1;                 
+                    disco nuevoDisco = new disco(rpta2[0], rpta2[1]+" "+rpta2[2], Double.valueOf(rpta2[lastPosition]) );
                     
                     discos.add(nuevoDisco);
                     }
@@ -99,10 +103,9 @@ public class HardwaredetectedApplication {
    
         } catch (IOException e) {
             e.printStackTrace();
-        }        
-        
-	}
-  @GetMapping(path = "/")
+        } 
+}
+  @GetMapping(path = "/hardware")
             public Map<String, Object> getdatos(){
                 Map<String, Object> hashMap = new HashMap<String, Object>();
                 hashMap.put("memorias", memorias);
@@ -111,4 +114,51 @@ public class HardwaredetectedApplication {
                
             return hashMap ;
            }
+            
+            @GetMapping(path = "/")
+            public String instalation(){         
+               return "ok" ;
+           }
+      
+            
+   @GetMapping(path = "/speedTest")  
+   
+         public ArrayList<String> results(){            
+        
+           try {
+            rpta.clear();
+            // Ruta completa al programa speedtest.exe en tu escritorio C:\Program Files (x86)\Hardware Detected by cfc\
+            String rutaSpeedTest ="C:\\Program Files (x86)\\Hardware Detected by cfc\\speedtest.exe";
+
+            // Crear un proceso que ejecute speedtest.exe
+            ProcessBuilder builder = new ProcessBuilder(rutaSpeedTest);
+            
+            // Iniciar el proceso
+            Process proceso = builder.start();
+            
+            // Capturar la salida del proceso
+            InputStream inputStream = proceso.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            
+            String linea;
+            boolean headerSkipped = false;
+            while ((linea = reader.readLine()) != null) {
+                if (!headerSkipped ||linea.length()==0 ){headerSkipped = true;continue;} 
+                // Procesar y mostrar la salida del programa speedtest.exe
+                rpta.add(linea);
+            }
+            
+            // Esperar a que el proceso termine
+            int exitCode = proceso.waitFor();
+            
+            // Mostrar el código de salida del proceso
+            System.out.println("El proceso terminó con código de salida: " + exitCode);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+        
+        
+            return rpta;
+    }
 }
